22eb057f02e9010a6065493502a1d2fabbb20598
Dan Sheppard <dans@dansheps.com>
2019-12-25 00:30:46 -0600

    Update US Localization

--- FistedDKP.lua
+++ FistedDKP.lua
@@ -1,3 +1,5 @@
 local _, fisted = ...;
 local _G = _G;
 local L = fisted.L;
+
+local version = "1.0.0-alpha"
---
+++ localization/en-us.lua
@@ -0,0 +1,102 @@
+
+
+local language = {
+    GUI = {
+        TITLE = "FistedDKP",
+    }
+    Raids = {
+        Encounters = {
+            MC = "Molten Core",
+            ONYXIA = "Onyxia's Lair",
+            BWL = "Blackwing Lair",
+            TAQ = "Temple of Ahn'Qiraj",
+            RAQ = "Ruins of Ahn'Qiraj",
+            ZG = "Zul'Gurub",
+            NAXX = "Naxxramas",
+            WORLD = "World Bosses"
+        },
+        Bosses = {
+            MC = {
+                LUCIFRON = "Lucifron",
+                MAGMADAR = "Magmadar",
+                GEHENNAS = "Gehennas",
+                GARR = "Garr",
+                GEDDON = "Baron Geddon",
+                SHAZZRAH = "Shazzrah",
+                SULFURON = "Sulfuron Harbinger",
+                GOLEMAGG = "Golemagg the Incinerator",
+                MAJORDOMO = "Majordomo Executus",
+                RAGNAROS = "Ragnaros"
+            },
+            ONYXIA = {
+                ONYXIA = "Onyxia"
+            },
+            BWL = {
+                RAZORGORE = "Razorgore the Untamed",
+                VAELASTRASZ = "Vaelastrasz the Corrupt",
+                BROODLORD = "Broodlord Lashlayer",
+                FIREMAW = "Firemaw",
+                EBONROC = "Ebonroc",
+                FLAMEGOR = "Flamegor",
+                CHROMAGGUS = "Chromaggus",
+                NEFARIAN = "Nefarian"
+            },
+            ZG = {
+                MANDOKIR = "Bloodlord Mandokir",
+                Gahzranka = "Gahz'ranka",
+                HAKKAR = "Hakkar",
+                THEKAL = "High Priest Thekal",
+                VENOXIS = "High Priest Venoxis",
+                ARLOKK = "High Priestess Arlokk",
+                JEKLIK = "High Priestess Jeklik",
+                JINDO = "Jin'do the Hexxer",
+                MARLI = "High Priestess Mar'li",
+                MADNESS = "Edge of Madness"
+            },
+            NAXX = {
+                ANUBREKHAN = "Anub'Rekhan",
+                FAERLINA = "Grand Widow Faerlina",
+                MAEXXNA = "Maexxna",
+                NOTH = "Noth the Plaguebringer",
+                HEIGAN = "Heigan the Unclean",
+                LOATHEB = "Loatheb",
+                RAZUVIOUS = "Instructor Razuvious",
+                GOTHIK = "Gothik the Harvester",
+                FOUR = "The Four Horsemen",
+                PATCHWERK = "Patchwerk",
+                GROBBULUS = "Grobbulus",
+                GLUTH = "Gluth",
+                THADDIUS = "Thaddius",
+                SAPPHIRON = "Sapphiron",
+                KELTHUZAD = "Kel'Thuzad"
+            },
+            AQ20 = {
+                AYAMISS = "Ayamiss the Hunter",
+                BURU = "Buru the Gorger",
+                RAJAXX = "General Rajaxx",
+                KURINNAXX = "Kurinnaxx",
+                MOAM = "Moam",
+                OSSIRIAN = "Ossirian the Unscarred"
+            },
+            AQ40 = {
+                SKERAM = "The Prophet Skeram",
+                SATURA = "Battleguard Sartura",
+                FANKRISS = "Fankriss the Unyielding",
+                HUHURAN = "Princess Huhuran",
+                EMPS = "Twin Emperors",
+                CTHUN = "C'Thun", 
+                BUG = "Bug Family",
+                VISCIDUS = "Viscidus",
+                OURO = "Ouro"
+            },
+            WORLD = {
+                AZUREGOS = "Azuregos",
+                KAZZAK = "Lord Kazzak",
+                EMERISS = "Emeriss",
+                LETHON = "Lethon",
+                YSONDRE = "Ysondre",
+                TAERAR = "Taerar"
+            }
+        }
+    }
+}
